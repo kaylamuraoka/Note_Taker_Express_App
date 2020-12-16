269fc3da2fcc6a0a02c2da97694970465c70dd46
kaylamuraoka <kmurs98@gmail.com>
2020-12-16 12:38:29 -1000

    set up html routes

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ const app = express();
 // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+const htmlRoutes = require("./routes/html.routes");
+
+app.use("/", htmlRoutes);
+
 // LISTENER
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
